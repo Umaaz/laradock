a1c393eaf522573ba5df90d9e4f5d195f003564d
Ben <ben_donnelly@intergral.com>
2019-10-22 13:03:44 +0200

    corrected aliases file for import in bashrc

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,10 @@
-export REPO_DIR="$(dirname $PWD)"
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "Using REPO_DIR: ${REPO_DIR}"
+export REPO_DIR="$DIR"
+
+# echo "Using REPO_DIR: ${REPO_DIR}"
 
 alias composer='docker exec laradock_workspace_1 composer --working-dir=/var/www/${PWD:${#REPO_DIR}}'
-
+alias larastart='docker-compose -f ${REPO_DIR}/docker-compose.yml up -d nginx'
+alias larastop='docker-compose -f ${REPO_DIR}/docker-compose.yml down'
