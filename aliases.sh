85bc065177ae5b0d0dc74081085f942294df838f
Ben <ben_donnelly@intergral.com>
2019-10-23 16:20:31 +0200

    change to php 7.1
    added larabuild and corrected some aliases

--- aliases.sh
+++ aliases.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-export REPO_DIR="$DIR"
+export REPO_DIR="$(dirname $DIR)"
 
 # echo "Using REPO_DIR: ${REPO_DIR}"
 
-alias composer='docker exec laradock_workspace_1 composer --working-dir=/var/www/${PWD:${#REPO_DIR}}'
-alias larastart='docker-compose -f ${REPO_DIR}/docker-compose.yml up -d nginx'
-alias larastop='docker-compose -f ${REPO_DIR}/docker-compose.yml down'
+alias composer='docker exec --user=laradock laradock_workspace_1 composer --working-dir=/var/www/${PWD:${#REPO_DIR}}'
+alias php='docker exec --workdir=/var/www/${PWD:${#REPO_DIR}} --user=laradock laradock_workspace_1 php'
+alias larastart='docker-compose -f ${REPO_DIR}/laradock/docker-compose.yml --project-directory=${REPO_DIR}/laradock up -d nginx'
+alias larastop='docker-compose -f ${REPO_DIR}/laradock/docker-compose.yml --project-directory=${REPO_DIR}/laradock down'
+alias larabuild='docker-compose -f ${REPO_DIR}/laradock/docker-compose.yml --project-directory=${REPO_DIR}/laradock up --build -d nginx'
