c951170ef1880d69ddd04969535e7c30ef5c299c
Ben <ben_donnelly@intergral.com>
2019-10-23 16:40:06 +0200

    added tty to composer an php aliases

--- aliases.sh
+++ aliases.sh
@@ -5,8 +5,8 @@ export REPO_DIR="$(dirname $DIR)"
 
 # echo "Using REPO_DIR: ${REPO_DIR}"
 
-alias composer='docker exec --user=laradock laradock_workspace_1 composer --working-dir=/var/www/${PWD:${#REPO_DIR}}'
-alias php='docker exec --workdir=/var/www/${PWD:${#REPO_DIR}} --user=laradock laradock_workspace_1 php'
+alias composer='docker exec -it --user=laradock laradock_workspace_1 composer --working-dir=/var/www/${PWD:${#REPO_DIR}}'
+alias php='docker exec -it --workdir=/var/www/${PWD:${#REPO_DIR}} --user=laradock laradock_workspace_1 php'
 alias larastart='docker-compose -f ${REPO_DIR}/laradock/docker-compose.yml --project-directory=${REPO_DIR}/laradock up -d nginx'
 alias larastop='docker-compose -f ${REPO_DIR}/laradock/docker-compose.yml --project-directory=${REPO_DIR}/laradock down'
 alias larabuild='docker-compose -f ${REPO_DIR}/laradock/docker-compose.yml --project-directory=${REPO_DIR}/laradock up --build -d nginx'
